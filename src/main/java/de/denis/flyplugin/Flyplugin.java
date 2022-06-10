14b63c124de90858e014975f277da5dac173f15a
Dedev0951 <92547138+Dedev0951@users.noreply.github.com>
2022-06-10 13:56:21 +0200

    Update Flyplugin.java

--- src/main/java/de/denis/flyplugin/Flyplugin.java
+++ src/main/java/de/denis/flyplugin/Flyplugin.java
@@ -1,6 +1,7 @@
 package de.denis.flyplugin;
 
 import de.denis.flyplugin.commands.FlyCommand;
+import de.denis.flyplugin.commands.FlySpeedCommand;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,6 +10,7 @@ public final class Flyplugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getCommand("flyspeed").setExecutor(new FlySpeedCommand());
         getCommand("fly").setExecutor(new FlyCommand());
     }
 }
